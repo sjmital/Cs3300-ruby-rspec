af514eae239cb5b6bd3676abbb5f1c4f36bf7b4f
sjmital <sjmital908@gmail.com>
2023-02-22 22:34:49 +0000

    Updated part1_spec.rb with requested comments

--- spec/part1_spec.rb
+++ spec/part1_spec.rb
@@ -3,17 +3,26 @@
 require_relative '../lib/ruby_intro'
 
 describe 'Ruby intro part 1' do
-  describe '#sum' do
+  describe '#sum' do # Defines the group of tests contained in its block as "sum"
 
+    # Prints a message if one of the contained tests fails
+    # "it" says that the following tests will print the given message on failure
     it 'returns correct sum [1 point]', points: 1 do
+      # Test passes if the result of adding those numbers is of Integer type
       expect(sum([1, 2, 3, 4, 5])).to be_a_kind_of Integer
+      # Test passes if the result of adding the numbers equals 15
       expect(sum([1, 2, 3, 4, 5])).to eq(15)
+      # Test passes if the result of adding the numbers equals 5
       expect(sum([1, 2, 3, 4, -5])).to eq(5)
+      # Test passes if the result of adding the numbers equals -90
       expect(sum([1, 2, 3, 4, -5, 5, -100])).to eq(-90)
     end
 
+    # Prints a message if one of the contained tests fails
     it 'works on the empty array [2 points]', points: 2 do
+      # Passes if the sum method doesn't throw an error
       expect { sum([]) }.not_to raise_error
+      # Passes if the results of the sum method is zero
       expect(sum([])).to be_zero
     end
   end
